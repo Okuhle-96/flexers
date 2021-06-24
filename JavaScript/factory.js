45b18a93f630e7c3fbdd66020dfba108ed05c3a9
Okuhle_Mhambi <bukhosimhambi@gmail.com>
2021-06-24 08:19:25 +0200

    wejhdg

---
+++ JavaScript/factory.js
@@ -0,0 +1,29 @@
+function doctors() {
+    var doctors =[{name: "Dr Mendu", field: "Dentistry", fee:300}, 
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300},  
+                 {name: "Dr Mendu", field: "Dentistry", fee:300} 
+ ]
+
+ function getDoctors() {
+return doctors;
+ }
+
+ return {
+     getDoctors
+ }
+}
